8012b5e68f73cac85f410585f0ee903a0c7b3cd9
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-09-20 18:35:39 +0300

    Указано полное имя класса для MyFirstProgram

--- sandbox/src/main/java/ru/gog/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/gog/sandbox/MyFirstProgram.java
@@ -1,6 +1,8 @@
+package ru.gog.sandbox;
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-	   System.out.println("Hello, world!");
-}
+        System.out.println("Hello, world!");
+    }
 }
