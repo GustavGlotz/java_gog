3719d374baf73b0522e1ecc00711a2133da5092c
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-10-05 19:22:46 +0300

    запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.gog.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.gog.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
 
     @BeforeMethod(alwaysRun = true)
