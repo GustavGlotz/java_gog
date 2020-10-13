3da1be684491a81bcab10837c0e5c295c1cb579a
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-10-13 17:19:20 +0300

    Сортировка списков(группы)

--- addressbook-web-tests/src/test/java/ru/gog/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/model/GroupData.java
@@ -15,6 +15,13 @@ public class GroupData {
         this.footer = footer;
     }
 
+    public GroupData(String name, String header, String footer) {
+        this.id = Integer.MAX_VALUE;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -22,22 +29,12 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (id != groupData.id) return false;
         return name != null ? name.equals(groupData.name) : groupData.name == null;
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
-    }
-
-    public GroupData(String name, String header, String footer) {
-        this.id = 0;
-        this.name = name;
-        this.header = header;
-        this.footer = footer;
+        return name != null ? name.hashCode() : 0;
     }
 
     @Override
