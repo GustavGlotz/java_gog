7ea248ded5221268f61207e50c28c939b8be9452
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-10-20 18:13:35 +0300

    Повсеместное использование уникальных идентификаторов объектов(создание контакта)

--- addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/ContactCreationTests.java
@@ -1,32 +1,27 @@
 package ru.gog.addressbook.tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import ru.gog.addressbook.model.ContactData;
 
-
-import java.util.Comparator;
-
-import java.util.List;
+import java.util.Set;
 
 public class ContactCreationTests extends TestBase {
 
 
     @Test
     public void testContactCreation() throws Exception {
-        List<ContactData> before = app.contact().list();
+        Set<ContactData> before = app.contact().all();
         app.contact().initContactCreation();
         ContactData contact = new ContactData()
                 .withFirstname("Тодд").withLastname("Говард").withEmail("govard@test.ru").withPhone("68445226");
         app.contact().create(contact);
-        List<ContactData> after = app.contact().list();
+        Set<ContactData> after = app.contact().all();
         Assert.assertEquals(after.size(), before.size() + 1);
 
 
+        contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt());
         before.add(contact);
-        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
-        before.sort(byId);
-        after.sort(byId);
         Assert.assertEquals(before, after);
     }
 }
