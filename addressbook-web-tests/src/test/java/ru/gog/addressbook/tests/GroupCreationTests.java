b768cffacc0a3163ee934dc0be5faadf0cfd8307
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-10-28 19:38:55 +0300

    Параметризация тестовых методов(группы)

--- addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/GroupCreationTests.java
@@ -1,20 +1,33 @@
 package ru.gog.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.gog.addressbook.model.GroupData;
 import ru.gog.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[]{new GroupData().withName("test1").withHeader("header 1").withFooter("footer 1")});
+        list.add(new Object[]{new GroupData().withName("test2").withHeader("header 2").withFooter("footer 2")});
+        list.add(new Object[]{new GroupData().withName("test3").withHeader("header 3").withFooter("footer 3")});
+        return list.iterator();
+    }
 
-    @Test
-    public void testGroupCreation() throws Exception {
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test1");
         app.group().create(group);
         Groups after = app.group().all();
         assertThat(after.size(), equalTo(before.size() + 1));
@@ -22,3 +35,5 @@ public class GroupCreationTests extends TestBase {
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 }
+
+
