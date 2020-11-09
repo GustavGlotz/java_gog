87748318611692efb63ef001b110064b9ef11b30
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-11-09 16:03:14 +0300

    +

--- addressbook-web-tests/src/test/java/ru/gog/addressbook/appmanager/DbHelper.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/appmanager/DbHelper.java
@@ -6,6 +6,7 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.gog.addressbook.model.ContactData;
+import ru.gog.addressbook.model.Contacts;
 import ru.gog.addressbook.model.GroupData;
 import ru.gog.addressbook.model.Groups;
 
@@ -31,4 +32,16 @@ public class DbHelper {
         session.close();
         return new Groups(result);
     }
+
+    public Contacts contacts() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery("from ContactData").list();
+        session.getTransaction().commit();
+        session.close();
+        return new Contacts(result);
+
+    }
+
+    
 }
