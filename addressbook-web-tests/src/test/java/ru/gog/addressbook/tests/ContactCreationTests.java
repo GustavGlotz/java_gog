ab496e70d38a168de66bc133c42912e4a7bffb5c
GustaveGlotz <68445226+GustaveGlotz@users.noreply.github.com>
2020-11-10 19:24:08 +0300

    Отключаемые проверки UI

--- addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/ContactCreationTests.java
@@ -47,5 +47,6 @@ public class ContactCreationTests extends TestBase {
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+        verifyContactListInUI();
     }
 }
--- addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/gog/addressbook/tests/GroupCreationTests.java
@@ -61,6 +61,7 @@ public class GroupCreationTests extends TestBase {
         assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+        verifyGroupListInUI();
 
     }
 }
